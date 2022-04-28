bf779b24ad711642bd72a88c73155c60d6a16206
dipinbajra <bajradipin98@gmail.com>
2022-04-28 14:03:37 +0545

    replace input text with a div and Reset button added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
 function calc() {
     var n1 = parseInt(document.getElementById('n1').value);
     var n2 = parseInt(document.getElementById('n2').value);
-    var oper = document.getElementById('operators').value;
+    var oper=document.getElementById('operators').value
 
     if (oper === '+') {
-        document.getElementById('result').value = n1 + n2;
+        document.getElementById('result').textContent = n1 + n2;
     }
 
     if (oper === '-') {
-        document.getElementById('result').value = n1 - n2;
+        document.getElementById('result').textContent = n1 - n2;
     }
 
     if (oper === '/') {
-        document.getElementById('result').value = n1 / n2;
+        document.getElementById('result').textContent = n1 / n2;
     }
 
     if (oper === '*') {
-        document.getElementById('result').value = n1 * n2;
+        document.getElementById('result').textContent = n1 * n2;
     }
 
 
+}
+
+function reset(){
+    document.getElementById("n1").value="";
+    document.getElementById("n2").value="";
+    document.getElementById("result").textContent="";
+    
 }
